55e7310c2447a44f01b7220ce6e998bcf50d9de9
rikvanderstelt <61784570+rikvanderstelt@users.noreply.github.com>
2020-05-25 23:01:54 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,34 @@
+import random
+import math
+
+class Board:
+    def __init__(self):
+        pass
+        #add the state of the board at the start
+    
+    def update_board(self):
+        pass
+        # Get the orders
+        # Do all the stuff here to update the board
+        # print the new map and a list of order results
+
+class Order
+#with a subclass definitive order?
+
+class Country(neighbour_list,coast_list):
+
+    def __init__(self):
+        self.neighbours = neighbour_list
+        self.coasts = coast_list
+
+    def get_neighbours(self):
+        return neighbour_list
+
+class Map
+# with method update map?
+
+
+
+
+
 
