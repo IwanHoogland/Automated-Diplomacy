d88516ff873317d1c78e92a0f3ffcd4ba81b30b1
IwanHoogland <66315536+IwanHoogland@users.noreply.github.com>
2020-06-02 17:07:12 +0200

    Update code.py
    Heb gekeken naar de province Class en wat misschien weg kan, doe propose file change nu ook als een soort 'save'

--- code.py
+++ code.py
@@ -12,60 +12,59 @@ import numpy as np
 
 class Province:
 
-    def __init__(self, name, is_sc = False, land_neighbour_list = [], coast_neighbours = {}):
+    def __init__(self, name, is_sc = False, neighbours = [], unit = False):
         '''
         is_sc is true if the province is a supply center, land_neighbour_list is a list of provinces, coast_neighbours a dictionary with
         the name of the coast as key and as value a list of the provinces bordering that coast.
         '''
+        # Why make coast_neighbours a dictionary and land_neighbour_list a list? Better to make all neighbour provinces 1 list?
+        
         self.name = name
-        self.land_neighbours = land_neighbour_list
-        self.coasts = coast_neighbours
-        self.units = dict.fromkeys(['land'] + list(self.coasts.keys()),False) 
+        self.neighbours = neighbours
+        self.units = unit # defining units in dictionary gives errors for 'empty' objects
         self.sc = is_sc
         
-        # self.units is a dictonary with as keys the different locations of units in a province (i.e. land and coast)
-        # and as value True or False depending on whether there is a unit there.
+        # simplified coast and land neighbours to a neighbours list
         
-    def __repr__(self):
+    def __repr__(self): # nice, kende __repr__ nog niet
         return self.name
     
     def is_sc(self):
         return self.sc
     
-    def get_land_neighbours(self):
-        return self.land_neighbours
+    def get_neighbours(self):
+        return self.neighbours
     
-    def get_coast_neighbours(self,coast_name):
-        return self.coasts[coast_name] 
+#   def get_coast_neighbours(self,coast_name):
+#       return self.coasts[coast_name] 
     
-    def add_land_neighbours(self,province_list):
-        for province in province_list:
-            if province not in self.land_neighbours:
-                self.land_neighbours.append(province)
+# Why introduce an add neighbour function? This should be given at initialisation.
+#     def add_land_neighbours(self,province_list):
+#        for province in province_list:
+#            if province not in self.land_neighbours:
+#                self.land_neighbours.append(province)
+# See add_land_neighbours
+#     def add_coast_neighbours(self,coast_name,province_list):
+#        if coast_name not in self.coasts.keys():
+#            self.coasts[coast_name] = []
+#            self.units = dict.fromkeys(['land'] + list(self.coasts.keys()),False)  # Add the coast to the locations where units can be
+#        for province in province_list:
+#            if province not in self.coasts[coast_name]:
+#                self.coasts[coast_name].append(province)
+# location not defined in class
+#     def add_unit(self,location):
+#        self.units[location] = True
+# location not defined in class
+#     def remove_unit(self,location):
+#         self.units[location] = False
     
-    def add_coast_neighbours(self,coast_name,province_list):
-        if coast_name not in self.coasts.keys():
-            self.coasts[coast_name] = []
-            self.units = dict.fromkeys(['land'] + list(self.coasts.keys()),False)  # Add the coast to the locations where units can be
-        for province in province_list:
-            if province not in self.coasts[coast_name]:
-                self.coasts[coast_name].append(province)
+#     def get_locations(self):
+#         return list(self.units.keys())
     
-    def add_unit(self,location):
-        self.units[location] = True
-    
-    def remove_unit(self,location):
-        self.units[location] = False
-    
-    def get_locations(self):
-        return list(self.units.keys())
-    
-    def get_unit_location(self):
-        for location in self.units.keys():
-            if self.units[location]:
-                return location
-                
-    
+#     def get_unit_location(self):
+#         for location in self.units.keys():
+#             if self.units[location]:
+#                 return location
     def get_units(self):
         return self.units
 
