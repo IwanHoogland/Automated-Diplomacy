75c45bd7c952e9f010bf7f89aaea90b714a2e369
IwanHoogland <66315536+IwanHoogland@users.noreply.github.com>
2020-06-02 15:32:59 +0200

    Update code.py
    added Province and Army class

--- code.py
+++ code.py
@@ -27,7 +27,36 @@ class Country(neighbour_list,coast_list):
 class Map
 # with method update map?
 
+class Province:
+    def __init__(self, name, army, hasSC):
+        self.name = name
+        self.army = army
+        self.hasSC = hasSC
+    def give_name(self):
+        print(f"This country is {self.name}")
+    def army_in(self):
+        print(f"This army is in {self.name}"  ) 
+    def army_enters(self, army):
+        if self.army == "":
+            self.army = army
+    def army_leaves(self):
+        self.army = "" 
+        
+ class Army:
+    def __init__(self, name, location, country):
+        self.name = name
+        self.location = location
+        self.country = country
+    
+    def occupies(self):
+        print("This province is occupied by " + self.name)
 
+    def move(self, location):
+        self.location = location
+ 
+
+        
+        
 
 
 
